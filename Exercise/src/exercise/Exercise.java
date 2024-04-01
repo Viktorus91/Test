0b373d2ea4558d75c10c203148b4023483f78787
vikto <vikto@Viktorus>
2024-04-01 06:50:09 +0300

    First Commit
    our commit.

--- Exercise/src/exercise/Exercise.java
+++ Exercise/src/exercise/Exercise.java
@@ -43,14 +43,14 @@ package exercise;
 		        String str1 = "a";
 		        String str2 = "b";
 		         
-		        if (str1 == str2) {
+		        if (str1. equals (str2)) {
 		            System.out.println("Строки идентичны");
 		        } else {
 		            System.out.println("Строки неидентичны");
 		            
 //Задан массив целых чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Необходимо написать программу, которая выведет в консоль все чётные числа. 		            
 		            for (int i = 1; i <= 10; i++)
-			            if (i % 2 == 0)
+			            if (i % 2.equals (0))
 			                System.out.println(i);
 		            
 
